eea5c0278c87e942b376cd588672118a0e838515
anne.mian.ren@gmail.com <anne.mian.ren@gmail.com@83de91d1-0c60-d2d8-9714-f6ac544b9f78>
2011-04-13 17:55:45 +0000

    user mgr

--- trunk/easytaxi/src/com/easytaxi/usermgr/service/LoginService.java
+++ trunk/easytaxi/src/com/easytaxi/usermgr/service/LoginService.java
@@ -4,6 +4,7 @@ package com.easytaxi.usermgr.service;
 import com.easytaxi.bo.Passenger;
 import com.easytaxi.bo.Taxi;
 import com.easytaxi.common.service.BaseService;
+import com.easytaxi.usermgr.bo.LoginRecord;
 import com.easytaxi.usermgr.dao.LoginRecordDao;
 import com.easytaxi.usermgr.dao.PassengerDao;
 import com.easytaxi.usermgr.dao.TaxiDao;
@@ -17,8 +18,48 @@ public class LoginService extends BaseService {
         this.passengerDao = passengerDao;
     }
 
+    public PassengerDao getPassengerDao() {
+        return this.passengerDao;
+    }
+
     public void setTaxiDao(TaxiDao taxiDao) {
         this.taxiDao = taxiDao;
+    }
+
+    public TaxiDao getTaxiDao() {
+        return this.taxiDao;
+    }
+
+    private Taxi getValidTaxi(String account, String password) {
+        boolean isEmailCheck = account.contains("@");
+        Taxi taxi = null;
+        try {
+            taxi = isEmailCheck ? taxiDao.getTaxiByEmail(account) : taxiDao.getTaxiByPlateNumber(account);
+            if (taxi != null && taxi.getPassword().equals(password)) {
+                return taxi;
+            } else
+                taxi = null;
+        } catch (Exception e) {
+            logger.error("Taxi check: account[" + account + "] password[" + password + "] error: ", e);
+        }
+        return taxi;
+    }
+
+    private Passenger getValidPassenger(String account, String password) {
+        boolean isEmailCheck = account.contains("@");
+        Passenger passenger = null;
+        try {
+
+            passenger = isEmailCheck ? passengerDao.getPassengerByEmail(account) : passengerDao
+                .getPassengerByPhone(account);
+            if (passenger != null && passenger.getPassword().equals(password)) {
+                return passenger;
+            } else
+                passenger = null;
+        } catch (Exception e) {
+            logger.error("Taxi check: account[" + account + "] password[" + password + "] error: ", e);
+        }
+        return passenger;
     }
 
     public String getValidUserid(String type, String account, String password) {
@@ -26,27 +67,47 @@ public class LoginService extends BaseService {
         if (account == null || account.equals(""))
             return userid;
         try {
-            boolean isEmailCheck = account.contains("@");
             if (type.equals("taxi")) {
-                Taxi taxi = null;
-                taxi = isEmailCheck ? taxiDao.getTaxiByEmail(account) : taxiDao.getTaxiByPlateNumber(account);
-                if (taxi != null && taxi.getPassword().equals(password))
+                Taxi taxi = getValidTaxi(account, password);
+                if (taxi != null)
                     userid = taxi.getUserid();
             } else {
-                Passenger passenger = null;
-                passenger = isEmailCheck ? passengerDao.getPassengerByEmail(account) : passengerDao
-                    .getPassengerByPhone(account);
-                if (passenger != null && passenger.getPassword().equals(password))
+                Passenger passenger = getValidPassenger(account, password);
+                if (passenger != null)
                     userid = passenger.getUserid();
             }
+            // if login successed, record login
+            if (!userid.equals(""))
+                recordLogin(userid, account);
         } catch (Exception e) {
             logger.error("check valid user error:", e);
         }
         return userid;
     }
 
-    public void recordLogin(String type, String account, String password) {
+    protected void recordLogin(String userid, String account) {
+        LoginRecord loginRecord = new LoginRecord(userid, account);
+        getLoginRecordDao().doSaveLoginRecord(loginRecord);
+    }
 
+    /**
+     * T010 get taxi detai info
+     * 
+     * @param userid
+     * @return
+     */
+    public Taxi getTaxiDetailInfo(String userid) {
+        return getTaxiDao().getTaxiByUserid(userid);
+    }
+
+    /**
+     * P010 get Passenger detail info
+     * 
+     * @param userid
+     * @return
+     */
+    public Passenger getPassengerDetailInfo(String userid){
+        return getPassengerDao().getPassengerByUserid(userid);
     }
 
     public void setLoginRecordDao(LoginRecordDao loginRecordDao) {
