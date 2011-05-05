3e674ebf88d29a2b8016c3e0cb0262c16b3dcec8
anne.mian.ren@gmail.com <anne.mian.ren@gmail.com@83de91d1-0c60-d2d8-9714-f6ac544b9f78>
2011-05-05 13:26:05 +0000

    register

--- trunk/easytaxi/src/com/easytaxi/usermgr/servlet/RegisterServlet.java
+++ trunk/easytaxi/src/com/easytaxi/usermgr/servlet/RegisterServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.easytaxi.bo.Passenger;
 import com.easytaxi.bo.Taxi;
+import com.easytaxi.common.SystemPara;
 import com.easytaxi.common.utils.BeanFactoryUtil;
 import com.easytaxi.usermgr.service.LoginService;
 
@@ -64,6 +65,7 @@ public class RegisterServlet extends HttpServlet {
             userid = doRegisterTaxi(taxi);
         }
         if (!userid.equals("")) {
+            request.getSession().setAttribute(SystemPara.SESSION_USERID, userid);
             response.sendRedirect("welcome.jsp?userId=" + userid);
         } else {
             response.sendRedirect("register.jsp");
